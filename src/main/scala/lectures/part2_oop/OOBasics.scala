ced4410b4473b52e4d95d2b83c3f272f5379f7c7
GonzaloAlcalaGD <galcala@griddynamics.com>
2023-02-02 14:46:09 -0600

    add method and method overloading examples

--- src/main/scala/lectures/part2_oop/OOBasics.scala
+++ src/main/scala/lectures/part2_oop/OOBasics.scala
@@ -2,4 +2,19 @@ package lectures.part2_oop
 
 object OOBasics extends App{
 
+  val person = new Person(name = "Gonzo", age = 22)
+  person.greet("Pepe")
+  person.greet()
 }
+
+//Constructor
+// class parameters are not FIELDS
+class Person(name: String, age: Int) {
+
+  // method
+  def greet(name: String): Unit = println(s"${this.name} say's hi to $name")
+  // overloading the method
+  def greet(): Unit = println(s"$name say's hi to $name")
+
+}
+
