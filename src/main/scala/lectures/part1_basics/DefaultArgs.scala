5a2a9f06266a3753b03d08003c23e55bd1acc4b5
GonzaloAlcalaGD <galcala@griddynamics.com>
2023-02-02 11:48:05 -0600

    add default argument example

--- src/main/scala/lectures/part1_basics/DefaultArgs.scala
+++ src/main/scala/lectures/part1_basics/DefaultArgs.scala
@@ -1,5 +1,15 @@
 package lectures.part1_basics
 
 object DefaultArgs extends App{
-  
+
+  /*
+  * Function that returns the factorial of a given number. Using tail recursion to prevent early
+  * stack overflow
+  * */
+  def trFactorial(number: Int, acc: Int = 1): Int = {
+    if (number <= 1) then acc
+    else trFactorial(number - 1, number * acc)
+  }
+
+  val fact10 = trFactorial(number = 10)
 }
