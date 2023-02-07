0d588504dd7068c4b970b1b5f4f2ce7020f519d8
GonzaloAlcalaGD <galcala@griddynamics.com>
2023-02-07 13:01:49 -0600

    fix typo

--- src/main/scala/lectures/part2_oop/AbstractDataTypes.scala
+++ src/main/scala/lectures/part2_oop/AbstractDataTypes.scala
@@ -23,7 +23,7 @@ object AbstractDataTypes extends App {
   }
 
   class Crocodile extends Animal with Carnivore {
-    override val creatureType: String = "croc"a
+    override val creatureType: String = "croc"
     def eat: Unit = println("nom nom nom")
     def eat(animal: Animal): Unit = println(s"I'm a croc and I'm eating ${animal.creatureType}")
   }
