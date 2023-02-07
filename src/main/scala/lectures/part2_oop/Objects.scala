be65cc1ac1d611f5451176a3c3f8594df2e35b86
GonzaloAlcalaGD <galcala@griddynamics.com>
2023-02-07 10:00:57 -0600

    add objects, classes and sigletons

--- src/main/scala/lectures/part2_oop/Objects.scala
+++ src/main/scala/lectures/part2_oop/Objects.scala
@@ -2,4 +2,24 @@ package lectures.part2_oop
 
 object Objects extends App {
 
+  // Scala does not have class-level functionality ("static")
+  // COMPANIONS
+  object Person {
+    // "static"/"class" - level functionality
+    val N_EYES = 2
+    def canFly: Boolean = false
+  }
+  class Person(name: String) {
+    // instance-level functionality
+  }
+  // COMPANIONS
+
+  println(Person.N_EYES)
+  println(Person.canFly)
+
+  // Scala objects are singleton instances by default
+  val mary = Person("Mary")
+  val john = Person("John")
+  println(mary == john)
+
 }
